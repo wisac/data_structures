f2344f20f2821d449b2d7fc4c4b92c6f9d0ec9f4
Isaac Wilson <iswilson777@gmail.com>
2023-04-01 18:48:25 +0000

    Implemented pushBack function

---
+++ singly_linked_list/pushBack.cpp
@@ -0,0 +1,61 @@
+#include "sLinkedList.h"
+
+/**
+ * pushBack - Insert a new node to the end of the list
+ * @newNode: Node to be inserted
+ * @newData: Data to be inserted
+ * 
+ */
+
+void sLinkedList::pushBack(Node *newNode)
+{
+    // tail = head so that we can use tail to tranverse the list without moving head
+    tail = head;
+
+    newNode->next = nullptr;
+
+    // if list is empty
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+
+    while (tail->next != nullptr)
+    {
+        tail = tail->next;
+    }
+
+    tail->next = newNode;
+}
+
+
+
+
+
+void sLinkedList::pushBack(const int newData)
+{
+
+    Node *newNode = new Node();
+
+    // tail = head so that we can use tail to tranverse the list without moving head
+    tail = head;
+
+    newNode->data = newData;
+
+    newNode->next = nullptr;
+
+//if list empty
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+
+    while (tail->next != nullptr)
+    {
+        tail = tail->next;
+    }
+
+    tail->next = newNode;
+}
