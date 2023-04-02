efe60bae20cd08893782218d62c3cadd7e7b95a4
Isaac Wilson <iswilson777@gmail.com>
2023-04-02 15:31:05 +0000

    feat: Implement method for removing first element in the list

---
+++ singly_linked_list/popFirst.cpp
@@ -0,0 +1,26 @@
+#include "sLinkedList.h"
+#include <iostream>
+
+/**
+ * popFirst - Removes the first element in the list
+ */
+
+void sLinkedList::popFirst()
+{
+    try
+    {
+
+        if (head == nullptr)
+        {
+            throw -1;
+        }
+
+        Node *temp = head->next;
+        delete(head) ;
+        head = temp; 
+    }
+    catch (const int emptyList)
+    {
+        std::cerr << "Error: Nothing to pop, List is empty\n";
+    }
+}
--- singly_linked_list/sLinkedList.h
+++ singly_linked_list/sLinkedList.h
@@ -8,17 +8,17 @@
  * @length : Length of the list
  *
  * @sLinkedList - No args constructor for initailizing   the list
- *
+ *printList - Prints all data in linked list
+ * @pushFront - Insert new node at the beginning of the list
+ * @pushAfter - Inserts a new node after a given node
  * @pushBack - Insert a new node to the end of the list
+ * 
  * @newNode: Node to be inserted
  * @newData: Data to be inserted
- * 
- * @pushFront - Insert new node at the beginning of the list
  * @newNode: Node to be inserted
  * @previousNode: Node to after which new node will be inserted
  * @newData: Data to be inserted
  *
- *printList - Prints all data in linked list
  *
  *
  *
@@ -39,7 +39,11 @@ public:
     void pushFront(const int newData);
     void pushAfter(Node *previousNode, Node *newNode);
     void pushAfter(Node *previousNode, const int newData);
+    
+    void popFirst();
+
     void printList();
+    
 };
 
 #endif
