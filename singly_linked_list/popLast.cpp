1701dc73b4787b17caa55e5cff6ca2fc43133f3f
Isaac Wilson <iswilson777@gmail.com>
2023-04-03 15:35:09 +0000

    update: Add exception handler for handling when list is empty

--- singly_linked_list/popLast.cpp
+++ singly_linked_list/popLast.cpp
@@ -3,21 +3,30 @@
 
 /**
  * popLast - Removes last element in the list
-*/
+ * Throws an exception of "Empty list" if list is empty
+ */
 
 void sLinkedList::popLast()
 {
+    try
+    {
+        if (head == nullptr)
+            throw 0;
 
         Node *first = head;
         Node *second = nullptr;
-    
+
         while (first->next->next != nullptr)
         {
-                first = first->next;
+            first = first->next;
         }
         second = first->next;
         first->next = nullptr;
-        delete(second);
-       
+        delete (second);
+    }
 
+    catch (int emptyList)
+    {
+        std::cerr << "Error: Cannot pop an empty list\n";
+    }
 }
